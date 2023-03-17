1f4a33a3ddaed70f662dc7a4f7e14be2a1432fc9
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2023-03-17 19:02:03 -0300

    se vinculo pehua phone con su respectivo json

--- products.js
+++ products.js
@@ -1,1 +1,7 @@
-https://my-json-server.typicode.com/SantiagoDelgadoo/Pehua-Phone/phone
+async function consumirApi (){ // creo una funcion asincrona a la cual le doy de nombre consumir api es async porque estoy consumiendo una api
+    let api = await fetch ('https://my-json-server.typicode.com/SantiagoDelgadoo/Pehua-Phone/phone') //declaro variable quue en este caso se llama api y despues la igualo a await para que espere la respuesta y fetch para que recupere la informacion del json y le paso el link de la api
+    console.log(api); //compruebo que el json este funcionando correctamente
+}
+consumirApi () //llamo a la funcion
+
+
